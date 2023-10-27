c6a4b36d81f6ad239cfa73622a0e1337147ce301
Giordano <zagattigiordano@hotmail.it>
2023-10-27 17:51:43 +0200

    column gap

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
 window.addEventListener("scroll", function () {
   let navbar = document.getElementById("navbar");
-  let button = document.getElementById("getstarted")
+  let button = document.getElementById("getstarted");
   let scrollTop = window.scrollY;
 
-  let header = document.getElementById("header");
-  let svg = document.getElementById("m-svg");
-
-  let scrollX = (header.scrollLeft / (header.scrollWidth - header.clientWidth)) * 100;
-  
-  svg.style.opacity = 1 - (scrollX / 100);
   if (scrollTop >= 236) {
     navbar.style.backgroundColor = "white";
     button.style.backgroundColor = "#1a8917";
   } else {
     navbar.style.backgroundColor = "#ffc017";
-    button.style.backgroundColor = "#191919"
+    button.style.backgroundColor = "#191919";
   }
 });
